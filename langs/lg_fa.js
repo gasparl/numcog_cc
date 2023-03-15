e5cdd323c94ab6b3f76c9c77f5f0a969416e7b49
Lilly Roth <97297097+lillyzoeroth@users.noreply.github.com>
2023-03-15 15:20:33 +0100

    Add files via upload

--- langs/lg_fa.js
+++ langs/lg_fa.js
@@ -164,7 +164,7 @@ const tt = {
     <input type="radio" id="loc2_fa" value="loc2_fa" name='loc2' />
     <label for="loc2_fa">ایران</label><br>
     <input type="radio" id="loc2_na" value="loc2_na" name='loc2' />
-    <label for="loc2_na">سایر (لطفا در پایین مشخص کنید) ><br>
+    <label for="loc2_na">سایر (لطفا در پایین مشخص کنید) </label><br>
     کشور دیگر:
     <input id="loc2_notes" style="width: 170px;margin:0;" maxlength="20">
     <hr>
